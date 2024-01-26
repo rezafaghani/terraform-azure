4755ba01b1a430e48e3dcbdc7e071f7d5c911744
Reza Faghani <rfaghani91@gmail.com>
2024-01-26 17:12:33 +0000

    add kuber terraform

---
+++ kubernetes/azure-aks.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "myAKSResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
